46d33ea4fce62613c7fdebf889dba5bfdf05a88d
David Klein <david.klein@psmtec.de>
2017-07-05 10:49:52 +0200

    add all logging functions to pipes

--- src/hxlog/LogPipe.hx
+++ src/hxlog/LogPipe.hx
@@ -1,11 +1,14 @@
 package hxlog;
 
+import haxe.PosInfos;
+
 @:final
 class LogPipe {
 
 	var _msg:LogMessage = new LogMessage();
 	var _bends:Array<LogBend> = [];
 	var _pipes:Array<LogPipe> = [];
+	var _filter:Int = 0xFFFFFFFF;
 
 	function new() {}
 
@@ -51,4 +54,34 @@ class LogPipe {
 		_bends.push(b);
 		return this;
 	}
+
+	function print(message:Any, level:LogLevel, ?infos:PosInfos) {
+		if (message == null || !level.check(_filter)) {
+			return;
+		}
+
+		_msg.text = message;
+		_msg.level = level;
+		_msg.infos = infos;
+		run();
+	}
+
+	public inline function filter(?levels:Array<LogLevel>) {
+		_filter = LogLevel.buildMask(levels);
+	}
+
+	public inline function trace(message:Any, ?position:PosInfos)
+		print(message, LogLevel.TRACE, position);
+
+	public inline function debug(message:Any, ?position:PosInfos)
+		print(message, LogLevel.DEBUG, position);
+
+	public inline function info(message:Any, ?position:PosInfos)
+		print(message, LogLevel.INFO, position);
+
+	public inline function warning(message:Any, ?position:PosInfos)
+		print(message, LogLevel.WARNING, position);
+
+	public inline function error(message:Any, ?position:PosInfos)
+		print(message, LogLevel.ERROR, position);
 }
