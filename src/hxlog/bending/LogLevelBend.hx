b921d020de0db598192d60bb6303a322b6ff9db8
David Klein <david.klein@psmtec.de>
2017-07-05 12:32:45 +0200

    customizable LogLevel names

--- src/hxlog/bending/LogLevelBend.hx
+++ src/hxlog/bending/LogLevelBend.hx
@@ -4,16 +4,11 @@ class LogLevelBend extends LogBend {
 
 	var _levels:Array<String>;
 
-	public function new() {
-		_levels = [];
-		_levels[LogLevel.TRACE] = "T/";
-		_levels[LogLevel.DEBUG] = "D/";
-		_levels[LogLevel.INFO] = "I/";
-		_levels[LogLevel.WARNING] = "W/";
-		_levels[LogLevel.ERROR] = "E/";
-	}
+	static var defaultTags = ["T/", "D/", "I/", "W/", "E/" ];
 
-	override public function bend(msg:LogMessage) {
-		msg.text = _levels[msg.level] + msg.text;
-	}
+	public function new( ?tags )
+		_levels = tags != null ? tags : defaultTags;
+
+	override public function bend(msg:LogMessage)
+		msg.text = '${_levels[msg.level]}${msg.text}';
 }
