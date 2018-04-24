47a75bbdb43f94572bea4b6d1c50d7cdd078d1e7
David Klein <david.klein@psmtec.de>
2018-04-24 14:07:28 +0200

    minor update
    remove trace nonsense
    add an `append` parameter to optionally keep the log contents (default is set to clear the file now)

--- src/hxlog/logging/NodeFileLog.hx
+++ src/hxlog/logging/NodeFileLog.hx
@@ -3,17 +3,18 @@ package hxlog.logging;
 class NodeFileLog extends LogBend {
 	var stream: Dynamic; // WriteStream
 
-	public function new( filename: String ) {
+	public function new( filename: String, append = false ) {
 		var fs = untyped __js__("require('fs');");
 		var path = untyped __js__("require('path');");
+
+		if (!append) {
+			fs.unlinkSync(path.resolve(untyped __js__('__dirname'), filename));
+		}
+
 		stream = untyped __js__("fs.createWriteStream(path.resolve(__dirname, filename), { autoClose: true });");
-		trace('---');
-		trace(stream);
-		trace('---');
 	}
 
 	override public function bend( msg: LogMessage ) {
 		stream.write('${msg.text}\n');
-		// Log.nativeTrace.haxeTrace(msg.text, msg.infos);
 	}
 }
