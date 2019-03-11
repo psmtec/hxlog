22e7689ea034fe963fc950f05a815e371b81b90d
David Klein <david.klein@psmtec.de>
2019-03-11 11:39:19 +0100

    make logging setup suck less

--- src/hxlog/logging/NodeFileLog.hx
+++ src/hxlog/logging/NodeFileLog.hx
@@ -3,23 +3,28 @@ package hxlog.logging;
 class NodeFileLog extends LogBend {
 	var stream: Dynamic; // WriteStream
 
-	public function new( filename: String, append = false ) {
-		var fs = untyped __js__("require('fs');");
-		var path = untyped __js__("require('path');");
+	public function new( path: String, filename: String, append = false ) {
+		trace('creating FileLog: $path/$filename');
+
+#if !macro
+		var fs = js.Syntax.code("require('electron').remote.require('fs');");
+		var p = js.Syntax.code("require('electron').remote.require('path');");
+		var url = js.Syntax.code("p.resolve(path, filename);");
 
 		if (!append) {
 			try {
-				fs.unlinkSync(path.resolve(untyped __js__('__dirname'), filename));
+				fs.unlinkSync(url);
 			} catch (x: Dynamic) {
-				trace(x);
+				// trace(x);
 			}
 		}
 
 		try {
-			stream = untyped __js__("fs.createWriteStream(path.resolve(__dirname, filename), { autoClose: true });");
+			stream = js.Syntax.code("fs.createWriteStream(url, { autoClose: true });");
 		} catch (x: Dynamic) {
 			trace(x);
 		}
+#end
 	}
 
 	override public function bend( msg: LogMessage ) {
